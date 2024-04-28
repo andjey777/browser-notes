908271c096dfe5eba41006ceb33c603e9df37728
andjey777 <andjey_777@mail.ru>
2024-04-28 17:59:51 +0300

    better css

--- notes/forms.py
+++ notes/forms.py
@@ -5,8 +5,10 @@ from notes.models import NotesModel
 
 
 class NotesForm(forms.Form):
-    name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Name"}))
-    notes_text = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Text"}))
+    name = forms.CharField(label="", widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Name"}))
+    notes_text = forms.CharField(
+        label="", widget=forms.Textarea(attrs={"class": "form-control", "placeholder": "Text"})
+    )
 
     class Meta:
         model = NotesModel
